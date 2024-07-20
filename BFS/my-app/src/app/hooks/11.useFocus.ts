8d8e3186dcb1c224f754d0709049827290962ac2
DoroninDmitrii <doronin_dima@mail.ru>
2024-07-20 11:42:21 +0300

    usefocus

---
+++ BFS/my-app/src/app/hooks/11.useFocus.ts
@@ -0,0 +1,30 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+
+export function useFocus<T extends HTMLElement>() {
+  const [focused, setFocused] = useState(false);
+  const ref = useRef<T>();
+
+  const onFocus = () => {
+    setFocused(true);
+  };
+
+  const onBlur = () => {
+    setFocused(false);
+  };
+
+  const callbackRef = useCallback((node: T) => {
+    if (ref.current) {
+      ref.current.removeEventListener('focus', onFocus);
+      ref.current.removeEventListener('blur', onBlur);
+    }
+
+    ref.current = node;
+    
+    if (ref.current) {
+      ref.current.addEventListener('focus', onFocus);
+      ref.current.addEventListener('blur', onBlur);
+    }
+  }, [])
+
+  return { focused, callbackRef };
+};
--- BFS/my-app/src/app/page.tsx
+++ BFS/my-app/src/app/page.tsx
@@ -4,6 +4,7 @@ import styles from "./page.module.css";
 import { useTimeout } from "./hooks/2.useTimeout";
 import { useIsFirstRender } from "./hooks/3.useIsFirstRender";
 import { useHover } from "./hooks/6.useHover";
+import { useFocus } from "./hooks/11.useFocus";
 
 export default function Home() {
   useTimeout(() => console.log('hello'), 2000);
@@ -11,14 +12,20 @@ export default function Home() {
 
   const hover1 = useHover();
   const hover2 = useHover();
+
+  const { callbackRef, focused } = useFocus();
+
   return (
     <main className={styles.main}>
-      <div ref={hover1.callbackRef}>
+      {/* <div ref={hover1.callbackRef}>
         {hover1.isHovered ? 'Hello World' : 'Goodbuy World'}
       </div>
       <div ref={hover2.callbackRef}>
         {hover2.isHovered ? '1' : '2'}
-      </div>
+      </div> */}
+
+      <input ref={callbackRef} />
+        {focused && 'Hello World'}
     </main>
   );
 }
